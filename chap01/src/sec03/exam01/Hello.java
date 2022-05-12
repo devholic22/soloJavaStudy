6c02187f542844003e603dae0dba017ff4fe1a53
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-05-12 11:31:52 +0900

    Initial Setting & 주석 사용하기

---
+++ chap01/src/sec03/exam01/Hello.java
@@ -0,0 +1,12 @@
+package sec03.exam01;
+/*
+작성자: 최현준
+작성일: 2022.05.12
+ */
+public class Hello {
+    // 프로그램 실행 진입점 (인텔리제이 단축어: psvm)
+    public static void main(String[] args) {
+        // 콘솔에 출력하는 실행문
+        System.out.println("Hello, Java");
+    } // end of main
+} // end of class
