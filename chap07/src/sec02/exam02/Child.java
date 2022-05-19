a69a30d24dacbf6f329908b429ce71d2c2ca0b11
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-05-19 12:49:39 +0900

    자동 타입 변환 후의 멤버 접근

---
+++ chap07/src/sec02/exam02/Child.java
@@ -0,0 +1,12 @@
+package sec02.exam02;
+
+public class Child extends Parent{
+    @Override // 재정의
+    public void method2(){
+        System.out.println("Child-method2()");
+    }
+
+    public void method3(){
+        System.out.println("Child-method3()");
+    }
+}
